b1a3abfca97d0b501af81f2145490264634ff47a
Rodr1goTavares <rodr1gowzxz2@gmail.com>
2025-03-13 22:02:28 -0300

    :sparkles: Intial commit.

---
+++ src/main/java/com/osint/LeakScan/LeakScanApplication.java
@@ -0,0 +1,13 @@
+package com.osint.LeakScan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeakScanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeakScanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/osint/LeakScan/LeakScanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osint.LeakScan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeakScanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
